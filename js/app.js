33b995644f49752d4680f8de5076a122bd2ea963
meatloaf111 <meatloaf111jp@gmail.com>
2018-09-06 00:29:11 +0900

    Add comments

--- js/app.js
+++ js/app.js
@@ -2,37 +2,37 @@
 /*
  * Create a list that holds all of your cards
  */
-var cards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
-    "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt", "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
-    "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+let cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
+    'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf',
+    'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
 
 /*
  * Initialize Variables
  */
 
-var openedcards = [];
-var targetLists = [];
-var moveCounter = document.querySelector('.moves');
-var moves = 0;
-var num_cards = 0;
-var stars = document.querySelectorAll('.stars i');
-var num_stars = 0;
-var start;
-var timer = 0;
+let openedcards = [];
+let targetLists = [];
+let moveCounter = document.querySelector('.moves');
+let moves = 0;
+let num_cards = 0;
+let stars = document.querySelectorAll('.stars i');
+let num_stars = 0;
+let start;
+let timer = 0;
 
 
-var restartbtn = document.querySelector('.restart');
+let restartbtn = document.querySelector('.restart');
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - shuffle the list of cards using the provided 'shuffle' method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -45,9 +45,12 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * generate Cards for players to play from the beggining
- */
+/**
+* @description Generate the cards so that plyaer can play
+* @constructor
+* @param cardList - list of the cards
+*/
+
 
 function generateCards(cardList) {
 
@@ -65,9 +68,11 @@ function generateCards(cardList) {
 
 generateCards(cards);
 
-/*
- * Timer Function
- */
+/**
+* @description Timer function to show the elapsed time
+* @constructor
+*/
+
 
 function countupTimer() {
     now = new Date();
@@ -95,13 +100,20 @@ restartbtn.addEventListener('click', restart);
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of 'open' cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ /**
+* @description lock the cards that are matched and check if the game ends.
+* @constructor
+* @param event - event
+*/
+
 function lockcards(evt) {
     openedcards[0].classList.add('match');
     openedcards[1].classList.add('match');
@@ -113,18 +125,30 @@ function lockcards(evt) {
     }
 }
 
+/**
+* @description End the game and open the modal window to show the results
+* @constructor
+*/
+
+
 function gameOver() {
     console.log('game is over');
     modalcontent = document.getElementById('modal-content');
-    modalcontent.style.display = "block";
+    modalcontent.style.display = 'block';
     modalcontent.getElementsByClassName('winscore')[0].innerText = num_stars;
     modalcontent.getElementsByClassName('timespent')[0].innerText = timer;
-    document.getElementById('modal-overlay').style.display = "block";
+    document.getElementById('modal-overlay').style.display = 'block';
 }
 
 
 button = document.querySelector('button');
 button.addEventListener('click', restart);
+
+/**
+* @description Restart the game
+* @constructor
+*/
+
 
 function restart() {
     console.log('game restart');
@@ -142,8 +166,8 @@ function restart() {
     document.querySelector('#timer').innerHTML = '00:00';
     moveCounter.innerText = moves;
 
-    document.getElementById('modal-content').style.display = "none";
-    document.getElementById('modal-overlay').style.display = "none";
+    document.getElementById('modal-content').style.display = 'none';
+    document.getElementById('modal-overlay').style.display = 'none';
     setTimeout(function () {
         existing_cards = document.querySelectorAll('.deck li');
         existing_cards.forEach(function (card) {
@@ -160,6 +184,11 @@ function restart() {
     }, 1000);
 }
 
+/**
+* @description Hide the cards when those are not matched
+* @constructor
+* @param event - event
+*/
 
 function hidecards(evt) {
     setTimeout(function () {
@@ -170,6 +199,11 @@ function hidecards(evt) {
     }, 1000);
 }
 
+/**
+* @description Decide the number of stars based on the number of moves
+* @constructor
+* @param count- number of moves
+*/
 function decideStars(count) {
     if (count <= 10) {
         num_stars = 3;
@@ -184,18 +218,23 @@ function decideStars(count) {
     }
 }
 
+/**
+* @description Check to see chosen cards match or not
+* @constructor
+* @param event - event
+*/
 function matchedCheck(evt) {
 
     /*
-    console.log("card1:" + openedcards[0].childNodes[1].className);
-    console.log("card2:" + openedcards[1].childNodes[1].className);
+    console.log('card1:' + openedcards[0].childNodes[1].className);
+    console.log('card2:' + openedcards[1].childNodes[1].className);
     */
 
     if (openedcards[0].childNodes[1].className === openedcards[1].childNodes[1].className) {
-        console.log("card matched!");
+        console.log('card matched!');
         lockcards(evt);
     } else {
-        console.log("card not matched!");
+        console.log('card not matched!');
         openedcards.forEach(function () {
             hidecards(evt);
         });
@@ -205,6 +244,11 @@ function matchedCheck(evt) {
     decideStars(moves);
 }
 
+/**
+* @description Display the card players chose
+* @constructor
+* @param event - event
+*/
 
 function displaySymbol(evt) {
     if (evt.target.localName === 'li') {
